15e80c99b4a7605888c8bb4441ce792877106b5c
jakwuh <jakwuh@yandex-team.ru>
2017-05-08 21:14:50 +0300

    Add caching

--- src/components/Bicycle.js
+++ src/components/Bicycle.js
@@ -3,6 +3,9 @@ import {SafeString} from 'handlebars/runtime';
 import partialTemplate from './Wheel.hbs';
 import template from './Bicycle.hbs';
 import {SSR_HASH_ATTR} from '../enum';
+import Cache from 'lru-cache';
+
+const cache = new Cache(100);
 
 export class Bicycle {
 
@@ -18,22 +21,24 @@ export class Bicycle {
         return `${this.count}-${this.chunks}`;
     }
 
-    getContext(context) {
-        const uid = this.getUID();
-        return Object.assign({SSR_HASH_ATTR, uid}, context);
-    }
-
     async render() {
+        const uid = this.getUID();
         const {count, chunks} = this;
         const parts = [];
+
+        if (cache.has(uid)) {
+            console.info('Cache hit.');
+            return cache.get(uid);
+        }
 
         for (let i = 0; i < chunks; ++i) {
             parts.push(await this.renderPartial({offset: count * i, count}));
         }
 
         const content = new SafeString(parts.join(''));
-        const html = template(this.getContext({content}));
-        return new SafeString(html);
+        const html = new SafeString(template({SSR_HASH_ATTR, content, uid}));
+        cache.set(uid, html);
+        return html;
     }
 
 }
